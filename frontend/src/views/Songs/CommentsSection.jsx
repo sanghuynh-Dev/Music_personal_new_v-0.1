ec1e0ae4dc226b353cddc824a6666036b0310d7a
Sang Huynh <sanghuynhkuro@gmail.com>
2026-07-08 03:57:48 +0700

    add spinner loading at comment song

--- frontend/src/views/Songs/CommentsSection.jsx
+++ frontend/src/views/Songs/CommentsSection.jsx
@@ -4,6 +4,7 @@ import { NavLink } from "react-router-dom";
 import { useAuth } from "../../contexts/AuthContext";
 import useCommentStore from "../../stores/commentStore";
 import songApi from "../../services/songApi";
+import Loading from "../../components/loading/Loading";
 import styles from './Songs.module.scss'
 
 function CommentsSection({ songId }) {
@@ -62,25 +63,31 @@ function CommentsSection({ songId }) {
             )}
             <ul className={styles.commentsList} id="comments-list-items">
                 {/* <!-- Dynamically populated via AJAX --> */}
-                { comments && comments.length > 0 ?(
-                    comments?.map(comment => (
-                        <li key={comment._id} className={styles.commentItem}>
-                            <img 
-                                src={comment.user?.avatar?.url || 'https://res.cloudinary.com/dqynaodv1/image/upload/v1781293476/955c965a3e831375a9fc2ed4e7599882_zlbj68.jpg'} 
-                                alt={comment.user?.username} 
-                                className={styles.commentItemAvatar}/>
-                            <div className={styles.commentItemContent}>
-                                <div className={styles.commentItemHeader}>
-                                    <NavLink to={`/profile/${comment.user?._id}`} className={styles.commentUsername}>{comment.user?.username || 'Deleted User'}</NavLink>
-                                    <span className={styles.commentTime}>{timeString(comment.createdAt)}</span>
-                                </div>
-                                <p className={styles.commentText}>{comment.content}</p>
-                            </div>
-                        </li>
-                    ))
-                ) : (
-                    <li className="comments-loading">Loading comments...</li>
-                )}
+                <>
+                    { !comments ? (
+                        <div className="comments-loading"><Loading /></div>
+                    ):(
+                        comments.length > 0 ?(
+                            comments?.map(comment => (
+                                <li key={comment._id} className={styles.commentItem}>
+                                    <img 
+                                        src={comment.user?.avatar?.url || 'https://res.cloudinary.com/dqynaodv1/image/upload/v1781293476/955c965a3e831375a9fc2ed4e7599882_zlbj68.jpg'} 
+                                        alt={comment.user?.username} 
+                                        className={styles.commentItemAvatar}/>
+                                    <div className={styles.commentItemContent}>
+                                        <div className={styles.commentItemHeader}>
+                                            <NavLink to={`/profile/${comment.user?._id}`} className={styles.commentUsername}>{comment.user?.username || 'Deleted User'}</NavLink>
+                                            <span className={styles.commentTime}>{timeString(comment.createdAt)}</span>
+                                        </div>
+                                        <p className={styles.commentText}>{comment.content}</p>
+                                    </div>
+                                </li>
+                            ))
+                        ) : (
+                            <li className="comments-loading">There are no comments for this song.</li>
+                        )
+                    )}
+                </>
             </ul>
         </div>
     );
